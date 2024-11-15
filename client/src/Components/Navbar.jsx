73b649a4c8e84324272c39b5d419f731a3522ac9
AryanGhatge <aughatge007@gmail.com>
2024-11-15 15:20:49 +0530

    updated navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import "../assets/css/navbar.css";
+
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-left">
-        <a href="" className="navbar-brand">
-          <img src="./images/logo.png" alt="logo" className="logo"/>
+        <a href="/" className="navbar-brand">
+          <img src="./images/logo.png" alt="Website Logo" className="logo" />
         </a>
       </div>
 
       <div className="navbar-right">
-        <a href="" className="navbar-link">
+        <a href="/about" className="navbar-link">
           About
         </a>
-        <a href="" className="navbar-link">
+        <a href="/login" className="navbar-link">
           Login
         </a>
-        <a href="" className="navbar-link">
+        <a href="/register" className="navbar-link">
           Register
         </a>
       </div>
